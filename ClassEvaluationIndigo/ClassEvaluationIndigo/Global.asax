54e80d3482aa2bba8432e70332789d07ac9eeccf
LuckyMathebula <Luckysbuda@gmail.com>
2017-06-30 11:32:51 +0200

    styled

---
+++ ClassEvaluationIndigo/ClassEvaluationIndigo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClassEvaluationIndigo.MvcApplication" Language="C#" %>
---
+++ ClassEvaluationIndigo/ClassEvaluationIndigo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClassEvaluationIndigo.Startup))]
+namespace ClassEvaluationIndigo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
